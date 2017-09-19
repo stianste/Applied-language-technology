216185c6f1ea53ca4236ae333c40a22b52aa6e47
stianste <stianste@stud.ntnu.no>
2017-09-19 15:43:11 +0200

    Start working on lexical weighting

--- phrase_extraction.py
+++ phrase_extraction.py
@@ -28,7 +28,12 @@ def phrase_extraction_algorithm(foreign_sentence, english_sentence, A):
         english_phrase = " ".join(english_sentence_split[i] for i in range(e_start, e_end+1))
         foreign_phrase = " ".join(foreign_sentence_split[i] for i in range(fs, fe+1))
 
-        E.add((foreign_phrase, english_phrase))
+        sub_alignments = [alignment for alignment in A \
+            if (e_start <= alignment[0] <= e_end) and (fs <= alignment[1] <= fe)]
+        sub_alignments = tuple(sub_alignments)
+
+        print(foreign_phrase, "--", english_phrase, sub_alignments)
+        E.add((foreign_phrase, english_phrase, sub_alignments))
         fe += 1
 
         if fe in f_aligned or fe == len(foreign_sentence_split):
@@ -95,6 +100,24 @@ def update_word_translation_counter(word_translation_counter_e_given_f,
     word_translation_counter_f_given_e.update([(foreign_sentence_split[unmatched_foreign_word], 'NULL')])
     english_word_counter.update(['NULL'])
 
+def all_words_aligned_to_word(i, alignment):
+  return sum([1 for a in alignment if a[0] == i])
+
+def sum_of_all_words_with_alignment(e_phrase, f_phrase, alignments, word_translation_probabilities):
+  total = 0
+  for alignment in alignments:
+    # TODO: Solve for NULL
+    total += word_translation_probabilities[(e_phrase[alignment[0]], f_phrase[alignment[1]])]
+
+  return total
+
+def lex(e_phrase, f_phrase, alignment, word_translation_probabilities):
+  lexical_score = 1
+  for i in range(len(e_phrase)):
+    lexical_score *= 1 / max(1, all_words_aligned_to_word(i, alignment))
+    lexical_score *= sum_of_all_words_with_alignment(e_phrase, f_phrase, alignment, word_translation_probabilities)
+
+  return lexical_score
 def main():
   # phrase_extraction_algorithm(english_sentences[0], german_sentences[0], alignments[0])
   # srctext = "michael assumes that he will stay in the house"
@@ -140,19 +163,22 @@ def main():
   print()
   print(word_translation_probabilities_f_given_e)
 
-  f_phrase_counter = Counter(f_phrase for (f_phrase,e_phrase) in phrase_pairs)
-  e_phrase_counter = Counter(e_phrase for (f_phrase,e_phrase) in phrase_pairs)
+  f_phrase_counter = Counter(f_phrase for (f_phrase,e_phrase,alignment) in phrase_pairs)
+  e_phrase_counter = Counter(e_phrase for (f_phrase,e_phrase,alignment) in phrase_pairs)
   phrase_pair_counter = Counter(phrase_pairs)
 
   for phrase_pair in phrase_pairs:
-    f_phrase, e_phrase = phrase_pair
+    f_phrase, e_phrase, alignment = phrase_pair
 
     phrase_pair_freq = phrase_pair_counter[phrase_pair]
     f_freq = f_phrase_counter[f_phrase]
     e_freq = e_phrase_counter[e_phrase]
     p_f_e = phrase_translation_probabilities(f_phrase_counter, f_phrase, phrase_pair_counter, phrase_pair)
     p_e_f = phrase_translation_probabilities(e_phrase_counter, e_phrase, phrase_pair_counter, phrase_pair)
+    lex_e_f = lex(e_phrase.split(), f_phrase.split(), alignment, word_translation_probabilities_e_given_f)
+    lex_f_e = lex(f_phrase.split(), e_phrase.split(), alignment, word_translation_probabilities_f_given_e)
 
-    # print("{} ||| {} ||| {} {} {} ||| {} {}".format(f_phrase, e_phrase, f_freq, e_freq, phrase_pair_freq, p_f_e, p_e_f))
+    print("{} ||| {} ||| {} {} {} ||| {} {} ||| {} {}".format(\
+        f_phrase, e_phrase, f_freq, e_freq, phrase_pair_freq, p_f_e, p_e_f, lex_f_e, lex_e_f))
 
 main()

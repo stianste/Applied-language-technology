e043b0dcd5a946003bc7d1530be87b397d091377
¨KrisKorrel¨ <Kristian.korrel@student.uva.nl>
2017-10-17 12:20:54 +0200

    Convert probs to log10, comment functions

--- Assignment 3/data_reader.py
+++ Assignment 3/data_reader.py
@@ -1,3 +1,9 @@
+import numpy as np
+
+# Read in the translation model file and return the translation model object.
+# Returns a dictionary with the tuple (german_phrase, english_phrase) as key
+# and the tuple (p_f_given_e, lex_f_given_e, p_e_given_f, lex_e_given_f) as value
+# The probabilities are in log10
 def read_translation_model():
   with open('Data/phrase-table', 'r') as translation_model_file:
     translation_model_lines = translation_model_file.read().splitlines()
@@ -5,12 +11,16 @@ def read_translation_model():
   translation_model = {}
   for translation_model_line in translation_model_lines:
     (german_phrase, english_phrase, probabilities, _, _) = translation_model_line.split(" ||| ")
-    (p_f_given_e, lex_f_given_e, p_e_given_f, lex_e_given_f, _) = map(float, probabilities.split(" "))
+    (p_f_given_e, lex_f_given_e, p_e_given_f, lex_e_given_f, _) = map(np.log10, map(float, probabilities.split(" ")))
 
     translation_model[(german_phrase, english_phrase)] = (p_f_given_e, lex_f_given_e, p_e_given_f, lex_e_given_f)
 
   return translation_model
 
+# Read in the reordering model file and return the reordering model object.
+# Returns a dictionary with the tuple (german_phrase, english_phrase) as key
+# and the tuple (mono_rtl, swap_rtl, disc_rtl, mono_ltr, swap_ltr, disc_ltr) as value
+# The probabilities are in log10
 def read_reordering_model():
   with open('Data/dm_fe_0.75', 'r') as reordering_model_file:
     reordering_model_lines = reordering_model_file.read().splitlines()
@@ -18,12 +28,13 @@ def read_reordering_model():
   reordering_model = {}
   for reordering_model_line in reordering_model_lines:
     (german_phrase, english_phrase, probabilities) = reordering_model_line.split(" ||| ")
-    (mono_rtl, swap_rtl, disc_rtl, mono_ltr, swap_ltr, disc_ltr) = map(float, probabilities.split(" "))
+    (mono_rtl, swap_rtl, disc_rtl, mono_ltr, swap_ltr, disc_ltr) = map(np.log10, map(float, probabilities.split(" ")))
 
     reordering_model[(german_phrase, english_phrase)] = (mono_rtl, swap_rtl, disc_rtl, mono_ltr, swap_ltr, disc_ltr)
 
   return reordering_model
 
+# Read a certain number of lines and add the values into a dict (ngram-model)
 def get_ngram_model(language_model_lines, start_index, end_index):
   ngram_model = {}
 
@@ -40,6 +51,9 @@ def get_ngram_model(language_model_lines, start_index, end_index):
 
   return ngram_model
 
+# Returns the 5 n-gram models.
+# Each model is a dictionary with the ngram as key and the tuple: (probability, backoff_weight) as value
+# Both the probability and backoff_weight are log10
 def read_language_model():
   with open('Data/file.en.lm', 'r') as language_model_file:
     language_model_lines = language_model_file.read().splitlines()
@@ -63,3 +77,9 @@ def read_language_model():
   ngram_5_model = get_ngram_model(language_model_lines, start_index, start_index + ngram_5_n_lines)
 
   return ngram_1_model, ngram_2_model, ngram_3_model, ngram_4_model, ngram_5_model
+
+read_language_model()
+print(1)
+read_translation_model()
+print(2)
+read_reordering_model()
